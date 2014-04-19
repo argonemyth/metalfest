21ac061f2a3e74bb6235eaccac9a786d967ee0e0
Fei Tan <fei@argonemyth.com>
2014-04-19 17:46:39 +0400

    Started to use Python's unittest framework; Added some very basic user cases in functiona_tests.py.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class HomePageTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_see_homepage(self):
+        # Alice want to find checking out all the metal festivals on a nice interative
+        # map.
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention 'MetalFest'
+        self.assertIn('MetalFest', self.browser.title)
+        self.fail('Finish the test!')
+
+    # She notices all the metal festivals happening around the globle in
+    # 2014 (77 total)
+
+    # She notices that all upcoming ones are marked in color and past ones are
+    # marked in grey
+
+    # She wants to check all the festivals happending in July and Auguest,
+    # so she set the Jul 1st and Aug 31st in the date filter box, and now she
+    # saw only the festivals that are happening in July and Auguest.
+
+    # She wants to continue filter out all the festivals that are outside of
+    # the Europe, so she selected 'Europe' in the continent filter box and
+    # now only the festivals in europe remains.
+
+
+if __name__ == '__main__':
+    unittest.main()
