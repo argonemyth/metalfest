0a2f07611e51c472c5720208e7eca362a857340c
Fei Tan <fei@argonemyth.com>
2014-05-18 20:08:50 +0400

    Added a meaningful user agent for musicbrainz api.

--- festivals/utils.py
+++ festivals/utils.py
@@ -5,7 +5,14 @@ def query_musicbrainz(uid="", fmt="json"):
     url = "http://musicbrainz.org/ws/2/artist/"
     params = {"inc": "url-rels",
               "fmt": "json"} 
-    r = requests.get(url + uid, params=params)
+    # musicbrainz wants meaning header
+    # http://musicbrainz.org/doc/XML_Web_Service/Rate_Limiting
+    headers = {
+        'User-Agent': 'MetalMap/0.5',
+        'From': 'hello@metalmap.com'  # This is another valid field
+    }
+
+    r = requests.get(url + uid, headers=headers, params=params)
     # print "requesting", r.url
 
     if r.status_code == requests.codes.ok:
